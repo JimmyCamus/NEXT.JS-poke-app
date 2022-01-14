0f2842707eff12a8d598b17d448106ae2ab583da
Jimmy Camus <jeremy.camus.varela@gmail.com>
2022-01-14 19:18:46 -0300

    Add responsive design.

--- components/Card.js
+++ components/Card.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Card = styled.section`
   background-color: #eee;
-  margin: 15px;
+  margin-top: 15px;
   padding: 20px 25px;
   width: 250px;
   box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.3);
@@ -17,6 +17,15 @@ const Card = styled.section`
     text-decoration: none;
     font-size: 18px;
     font-weight: 500;
+  }
+
+  @media screen and (max-width: 768px) {
+    & {
+      width: 200px;
+    }
+    & > p {
+      font-size: 14px;
+    }
   }
 `;
 
--- components/Title.js
+++ components/Title.js
@@ -3,6 +3,10 @@ import styled from "styled-components";
 const Title = styled.h1`
   color: #000;
   font-size: 30px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 20px;
+  }
 `;
 
 export default Title;
